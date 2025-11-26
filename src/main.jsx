20a232d156aa40592b02bcdcec0159b2225bc251
prosuggesto <pro.suggesto.ai@gmail.com>
2025-11-26 12:54:42 +0100

    Debug: Add global try-catch

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StrictMode>,
-)
+try {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </StrictMode>,
+  )
+} catch (e) {
+  document.body.innerHTML = `<div style="color:red; padding:20px;"><h1>Critical Error</h1><pre>${e.toString()}</pre></div>`
+}
